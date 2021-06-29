ea2e9e949bb520cc54e1dbc9f466d971a6b6d287
Felipe Lourenco <felipe.v.lourenco@gmail.com>
2021-06-29 15:54:38 +0200

    chore: adding testing setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const App = (): React.ReactElement => {
+  return <h1>Hello world React!</h1>
+}
+
+export default App
---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import App from '../App'
+
+describe('App component', () => {
+  test("App contains correct text", () => {
+    const component = render(<App />)
+    const expectedText = "Hello world React!"
+    const foundText = component.getByText(expectedText)
+    expect(foundText.textContent).toContain(expectedText)
+  })
+})
